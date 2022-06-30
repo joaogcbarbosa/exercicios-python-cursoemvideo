7fff0df032187f1cab3272a0514eb64f01a5411f
João Gabriel Barbosa <joaogcb29@gmail.com>
2022-06-29 23:00:04 -0300

    Update Desafio060.py
    Adicionada outra resolução

--- 6 - Repetições em Python (while)/Desafio060.py
+++ 6 - Repetições em Python (while)/Desafio060.py
@@ -1,9 +1,18 @@
 #fatorial
-fat = 1
+
+"""fat = 1
 c = 2
 num = int(input('Digite um número para ver o fatorial: '))
 num += 1
 while num >= c:
     num -= 1
     fat *= num
+print(f'O fatorial é {fat}')""" #Resolução antiga
+
+
+fat = c = 1
+num = int(input('Digite um número para ver o fatorial: '))
+while num > c:
+    fat *= num
+    num -= 1
 print(f'O fatorial é {fat}')
