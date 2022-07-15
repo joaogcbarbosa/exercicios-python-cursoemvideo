7c9554f7bd643a02b2f57f3b4a6f3a25f3b8e7d8
João Gabriel Barbosa <joaogcb29@gmail.com>
2022-07-15 18:11:10 -0300

    Adição de outra resolução

--- 8 - Listas/Desafio078.py
+++ 8 - Listas/Desafio078.py
@@ -24,3 +24,26 @@ print(f'O menor valor inserido foi {menor}')
 for pos, valor in enumerate(valores):
     if valor == menor:
         print(f'{menor} na posição {pos}')
+
+# OUTRA RESOLUÇÃO:
+
+valores = []
+
+for c in range(0, 5):
+    valores.append(int(input(f'{c + 1}º número: ')))
+
+print()
+
+maior = max(valores)
+menor = min(valores)
+
+print(f'O maior número é {maior}')
+print(f'O menor número é {menor}')
+
+print()
+
+for c in range(0, 5):
+    if valores[c] == maior:
+        print(f'{maior} encontrado na posição {c + 1}')
+    if valores[c] == menor:
+        print(f'{menor} encontrado na posição {c + 1}')
