4cf66f64c15c4f622caa9c6c0403638fad0a6036
João Gabriel Barbosa <joaogcb29@gmail.com>
2022-09-03 14:41:28 -0300

    Alteração no código
    Retirada a variável "c" e a instrução "continue".

--- 08 - Listas/Desafio079.py
+++ 08 - Listas/Desafio079.py
@@ -1,14 +1,12 @@
 #Crie um programa onde o usuário possa digitar vários valores númericos e cadastre-os em uma lista. Caso o número já exista lá dentro, ele não será adicionado. No final, serão exibidos todos os valores únicos digitados, em ordem crescente.
 
 nums = []
-c = 0
 while True:
     num = int(input('Digite um número: '))
     if num not in nums:
         nums.append(num)
     else:
         print('Número já inserido anteriormente. Não computado.')
-        continue
     nums.sort()
     escolha = str(input('Deseja continuar[S/N]? ')).strip().upper()[0]
     while escolha not in 'SN':
