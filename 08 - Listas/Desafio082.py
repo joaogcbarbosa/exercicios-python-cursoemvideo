0c933606ba4befecad90843bcdb0d4cb6fe1b577
João Gabriel Barbosa <joaogcb29@gmail.com>
2022-09-03 15:04:39 -0300

    Alteração no nome da variável

--- 08 - Listas/Desafio082.py
+++ 08 - Listas/Desafio082.py
@@ -1,14 +1,14 @@
 #Crie um programa que vai ler vários números e colocar em uma lista. Depois disso, crie duas listas extras que vão conter apenas os valores pares e ímpares digitados, respectivamente. Ao final, mostre o conteúdo das três listas geradas.
 
 lista = []
-lsita_par = []
+lista_par = []
 lista_imp = []
 c = 0
 
 while True:
     lista.append(int(input('Digite um número: ')))
     if lista[c] % 2 == 0:
-        lsita_par.append(lista[c])
+        lista_par.append(lista[c])
     else:
         lista_imp.append(lista[c])
     c += 1
@@ -19,5 +19,5 @@ while True:
         break
 
 print(f'Lista completa: {lista}')
-print(f'Lista de números pares: {lsita_par}')
+print(f'Lista de números pares: {lista_par}')
 print(f'Lista de números ímpares: {lista_imp}')
